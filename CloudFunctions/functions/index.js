635a094d582763478207d66d81e4a0e651dff545
Marco Bongiovanni <marco.bongiovanni94@gmail.com>
2017-09-23 19:12:15 +0200

    installato lodash in cloud functions , e settato la prima funzione di sample

--- CloudFunctions/functions/index.js
+++ CloudFunctions/functions/index.js
@@ -1,12 +1,5 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
-
 const admin = require('firebase-admin');
 const _ = require('lodash');
 
@@ -33,7 +26,9 @@ exports.sendNewMessageNotification =
             console.log(result)
         })
         .catch((error)=>{console.log(error)})
-    
+    });
+
+
 
     // const getValuePromise = admin.database()
     //                             // .ref('messages')
@@ -59,11 +54,6 @@ exports.sendNewMessageNotification =
     //         return admin.messaging()
     //                 .sendToTopic('main', payload);
     //     });
-    });
-
-
-
-
 
  
 
